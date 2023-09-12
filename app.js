81e05cb9a6113b34d6a90dc51157570b19f24ad0
Andres Felipe <afgaleanob@gmail.com>
2023-09-12 15:14:05 -0500

    basc server

---
+++ app.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const { Sequelize } = require('sequelize');
+require('dotenv').config();
+
+const port = process.env.PORT || 3000;
+const mainRouter = require('./routes/index');
+
+const app = express();
+
+//views
+app.set('view engine', 'ejs');
+app.set('views', 'views' );
+
+//to get request params
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// routes
+app.use('/', mainRouter);
+
+//public
+app.use( express.static( process.env.PUBLICDIR ));
+
+app.listen(port, () => {
+    console.log(`Servidor en ejecución en el puerto ${port}.`);
+});
+
+//global errors
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send('Error interno del servidor');
+});
