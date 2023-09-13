e409586c332b97ce96d2468415115a1bd77730ca
Andres Felipe <afgaleanob@gmail.com>
2023-09-13 16:03:35 -0500

    signIn and signUp routes

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,17 @@
 const express = require('express');
 const router = express.Router();
 
+
+router.get('/sign-in', (req,res) => {
+    res.locals.pageTitle = 'Iniciar sesion';
+    res.locals.pageDescription = "";
+    res.render('signIn');
+
+});
+router.get('/sign-up', (req,res) => {
+    res.locals.pageTitle = 'Registrate';
+    res.locals.pageDescription = "";
+    res.render('signUp');
+})
+
 module.exports = router;
